6b002c9206564563da67d5f81e337ebc4acf66d1
TuxieNight <phowardsmith@wisc.edu>
2026-07-05 14:32:56 -0500

    made landing page layout more responsive

--- asthma-app/frontend/src/components/pages/LandingPage.jsx
+++ asthma-app/frontend/src/components/pages/LandingPage.jsx
@@ -1,23 +1,17 @@
-import { Badge, Button, Card } from 'react-bootstrap'
+import { Button, Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import LandingContent from "../landing/LandingContent";
 
 function LandingPage({ onLogin, onSignUp }) {
   return (
-    <div
-      className="vertical at-top-center p-5 green-body"
-      style={{ minHeight: "100vh", justifyContent: "space-between" }}
-    >
-      <div className="vertical at-top-center">
-        <h1>Lobelia</h1>
-        <h2 className="section-text">AI-Powered Asthma Risk Forecasting</h2>
-      </div>
-
-      <div className="horizontal-48 at-middle-center mt-3">
-        <Button className="button-dark btn-large-text" onClick={onLogin}>Login</Button>
-        <p className="btn-large-text">OR</p>
-        <Button className="button-dark btn-large-text" onClick={onSignUp}>Sign Up</Button>
-      </div>
-    </div>
-  )
+    <Container fluid className="min-vh-100 p-0">
+      <Row className="g-0">
+        <Col>
+          <LandingContent onLogin={onLogin} onSignUp={onSignUp} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
