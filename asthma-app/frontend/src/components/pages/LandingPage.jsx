574cc5b79efe3b63487fb21511106a13c6a3bd89
TuxieNight <phowardsmith@wisc.edu>
2026-07-06 18:16:14 -0500

    Finished responsive design of signup and login versions of landing page

--- asthma-app/frontend/src/components/pages/LandingPage.jsx
+++ asthma-app/frontend/src/components/pages/LandingPage.jsx
@@ -1,11 +1,23 @@
 import { Button, Container, Row, Col } from "react-bootstrap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LandingContent from "../landing/LandingContent";
 import AuthSlide from "../landing/AuthSlide";
+import useMediaQuery from "../../helper-functions/useMediaQuery";
+import { BREAKPOINTS } from "../../lib/constants";
 
 function LandingPage() {
   const [showLogin, setShowLogin] = useState(false);
   const [showSignUp, setShowSignUp] = useState(false);
+
+  const onLogin = () => setShowLogin(true);
+  const onSignUp = () => setShowSignUp(true);
+  const onBack = () => {
+    setShowLogin(false);
+    setShowSignUp(false);
+  }
+
+  // get lg breakpoint
+  const isLargeScreen = useMediaQuery(`(min-width: ${BREAKPOINTS.lg}px)`);
 
   return (
     <Container fluid className="min-vh-100 p-0">
@@ -13,21 +25,30 @@ function LandingPage() {
       {!showSignUp && !showLogin ? (
         <Col xs={12}>
           <LandingContent
-            onLogin={() => setShowLogin(true)}
-            onSignUp={() => setShowSignUp(true)}
+            onLogin={onLogin}
+            onSignUp={onSignUp}
+            onBack={onBack}
+            authSlideOpen={false}
           />
         </Col>
       ) : (
         <>
           <Col lg={6} className="d-none d-lg-flex">
             <LandingContent
-              onLogin={() => setShowLogin(false)}
-              onSignUp={() => setShowSignUp(false)}
+              onLogin={onLogin}
+              onSignUp={onSignUp}
+              onBack={onBack}
+              authSlideOpen={true}
             />
           </Col>
 
           <Col xs={12} lg={6}>
-            <AuthSlide showLogin={showLogin} showSignUp={showSignUp} />
+            <AuthSlide
+              showLogin={showLogin}
+              showSignUp={showSignUp}
+              onBack={onBack}
+              landingVisible={isLargeScreen}
+            />
           </Col>
         </>
       )}
