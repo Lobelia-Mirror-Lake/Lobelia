9ccf4904bea24c255ae009e4b29a00ede8bead04
TuxieNight <phowardsmith@wisc.edu>
2026-07-06 13:49:08 -0500

    began signup/login page

--- asthma-app/frontend/src/components/pages/LandingPage.jsx
+++ asthma-app/frontend/src/components/pages/LandingPage.jsx
@@ -1,23 +1,37 @@
 import { Button, Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import LandingContent from "../landing/LandingContent";
+import AuthSlide from "../landing/AuthSlide";
 
 function LandingPage() {
-  function onLogin() {
-
-  }
-
-  function onSignUp() {
-    
-  }
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
 
   return (
     <Container fluid className="min-vh-100 p-0">
-      <Row className="g-0">
-        <Col>
-          <LandingContent onLogin={onLogin} onSignUp={onSignUp} />
+      <Row className="g-0 min-vh-100">
+      {!showSignUp && !showLogin ? (
+        <Col xs={12}>
+          <LandingContent
+            onLogin={() => setShowLogin(true)}
+            onSignUp={() => setShowSignUp(true)}
+          />
         </Col>
-      </Row>
+      ) : (
+        <>
+          <Col lg={6} className="d-none d-lg-flex">
+            <LandingContent
+              onLogin={() => setShowLogin(false)}
+              onSignUp={() => setShowSignUp(false)}
+            />
+          </Col>
+
+          <Col xs={12} lg={6}>
+            <AuthSlide showLogin={showLogin} showSignUp={showSignUp} />
+          </Col>
+        </>
+      )}
+    </Row>
     </Container>
   );
 }
