208840fa145a06ee709e561135670b979e2ab707
TuxieNight <phowardsmith@wisc.edu>
2026-07-28 10:29:57 -0500

    Added landing page image

--- asthma-app/frontend/src/components/landing/LandingContent.jsx
+++ asthma-app/frontend/src/components/landing/LandingContent.jsx
@@ -1,6 +1,7 @@
 import { Button, Container, Row, Col } from "react-bootstrap";
 import { motion } from "framer-motion";
 import { useRef, useLayoutEffect, useState } from "react";
+import image from "../../assets/images/lungFlowers.png";
 
 function LandingContent({
   onLogin,
@@ -51,7 +52,7 @@ function LandingContent({
   return (
     <Container
       fluid
-      className="green-body vertical min-vh-100 h-100"
+      className="green-body vertical-16 min-vh-100 h-100"
       style={{
         justifyContent: "space-between"
       }}
@@ -67,6 +68,26 @@ function LandingContent({
           </h2>
         </Col>
       </Row>
+
+      <div
+        style={{
+          flex: 1,
+          position: "relative",
+        }}
+      >
+        <img
+          src={image}
+          alt=""
+          style={{
+            position: "absolute",
+            inset: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "contain",
+            pointerEvents: "none",
+          }}
+        />
+      </div>
 
       <motion.div
         animate={{
